d7236892f572ba4282e160f45569c3f0df5c7dc9
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-07-02 21:18:28 -0400

    Create Date.cpp

---
+++ Date/Date.cpp
@@ -0,0 +1,13 @@
+/*  name: jianxin zhu
+    date: july/02/2020
+*/
+
+#include"Date.h"
+
+int main()
+{
+	Date calendar{ 12,18,2020 };
+	Date calendar2{ 04,07,2020 };
+
+	calendar.displayDate(calendar2);
+}
